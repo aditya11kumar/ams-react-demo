2f3c47869baf45e2dae4ec5b47db70697c34b679
Aditya Kumar <aditya.kumar04@nagarro.com>
2023-05-24 10:39:28 +0530

    data deleted

--- reactform/reactform/src/index.js
+++
@@ -1,6 +0,0 @@
-import ReactDom from "react-dom";
-import App from './components/App';
-import './index.css';
-import './components/navbar.css';
-
-ReactDom.render(<App/>,document.getElementById('root'));
