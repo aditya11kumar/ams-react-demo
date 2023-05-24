ae711dfbed670a65c76a625f04a78fde5266e609
Aditya Kumar <aditya.kumar04@nagarro.com>
2023-05-24 21:36:22 +0530

    form updated

--- form/src/Navbar.js
+++ form/src/Navbar.js
@@ -1,6 +1,16 @@
 export default function Navbar() {
     return (<nav className="nav">
-        <div className="box"><h1>Traini Form</h1></div>
+        <div className="box">
+        <img src="img_girl.jpg" alt="Girl in a jacket" />
+            <h1>Dealer Management System</h1>
+            <div className="login">
+                <h2>Akhilesh Sharma</h2>
+                <img src="img_girl.jpg" alt="image" style={{padding: '20px'}}/>
+            </div>
+
+
+        
+        </div>
         {/* <a href="/" className="site-title">Trainee Form</a> */}
         <div className="list">
         <ul >
